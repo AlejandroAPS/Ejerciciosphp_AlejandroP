c60a45b87a51634cf286ec585607ceefe8d9dfd8
AlejandroAPS <alejandroagustin.perezserrano.25@campusfp.es>
2026-01-08 14:37:09 +0100

    Empezado el tercero
    Creada la idea y el html ,queda el codigo en php para poder encajar todo bien

---
+++ Hoja_ejercicios_php4/Generador_emails_corporativos3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method = "post">
+        <p>
+            <P><strong>Nombre:</strong><input type="text" name="nombre"required></P>
+            <P><strong>Apellido:</strong><input type="text" name="user" required></p>
+            <P><strong>Dominio:</strong><input type="text" name="user" required></P>
+        </p>
+        <br>
+        <p>
+            <input type = "submit" value="Enviar" size="20">
+        </p>
+    </form>
+    <?php
+    
+
+?>
+</body>
+</html>
--- Hoja_ejercicios_php4/PiedraPapelTijera2.php
+++ Hoja_ejercicios_php4/PiedraPapelTijera2.php
@@ -11,15 +11,15 @@
         <p>
             <strong>Escoge:</strong>
             <label>
-                <input type="radio" name="user" value="Piedra">
+                <input type="radio" name="user" value="piedra🪨" required>
                 Piedra<img src="..//Hoja_ejercicios_php4/Piedra.avif" width=100px height=100px>
             </label>
             <label>
-                <input type="radio" name="user" value="Papel">
+                <input type="radio" name="user" value="papel📄" required>
                 Papel<img src="..//Hoja_ejercicios_php4/Papel.jpg" width=100px height=100px>
             </label>
             <label>
-                <input type="radio" name="user" value="Tijera">
+                <input type="radio" name="user" value="tijera✂️" required>
                 Tijera<img src="..//Hoja_ejercicios_php4/Tijera.avif" width=100px height=100px>
             </label>
         </p>
@@ -29,22 +29,37 @@
         </p>
     </form>
     <?php
-        $usuario = $_POST["user"];
-        $CPU = random_int(1,3);
-        switch($numero){
+        $usuario = $_POST["user"];  //se recoge lo enviado por usuario
+        $CPU = random_int(1,3);     //Se genera el numero aleatorio
+        switch($CPU){
             case 1:
-                $jugada = "piedra";
+                $CPU = "piedra🪨";
                 break;
             case 2:
-                $jugada = "papel";
+                $CPU = "papel📄";
                 break;
             case 3:
-                $jugada = "tijera";
+                $CPU = "tijera✂️";
                 break;
-
-        };
-            
-
+        };  //Se asigna el numero generado con cada posibilidad
+        if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        if ($usuario === $CPU){
+            $resultado = "Empate";
+        }   //Compara si son identicos y marca empate
+        elseif(
+            ($usuario === "piedra🪨" && $CPU === "tijera✂️")||
+            ($usuario === "tijera✂️" && $CPU === "papel📄")||
+            ($usuario === "papel📄" && $CPU === "piedra🪨")
+        ){
+            $resultado = "¡Ganaste!";
+        }
+        else{
+            $resultado = "Perdiste";
+        }
+        
+        echo "Escogiste:$usuario <br> Tu rival escogio:$CPU<br>";
+        echo "$resultado";
+        }
     ?>
     
 </body>
