ae44fda32e75478c9778f428072cffdd74c2250c
AlejandroAPS <alejandroagustin.perezserrano.25@campusfp.es>
2026-02-06 00:06:40 +0100

    lol xd

--- Ejemplo_Modulo8 MVC/app/Vistas/Contactos/listar.php
+++ Ejemplo_Modulo8 MVC/app/Vistas/Contactos/listar.php
@@ -1,3 +1,16 @@
 <?php
     if (!empty($error)): 
 ?>
+    <div class="error"><?php echo htmlspecialchars($error);?></div>
+    <?php endif ?>
+    <div class="tarjeta">
+        <h2>Listado de contactos</h2>
+
+
+    <?php if (empty($contactos)): ?>
+        <p>No hay contactos todavía.</p>
+    <?php else:?>
+        <table>
+            
+        </table>
+
